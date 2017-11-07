4f442645bbb049447619200550baaa833adba859
Michael Rapaport <rapaport1995@gmail.com>
2017-11-06 22:09:14 -0500

    Fixing merge conflict in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :parents
   resources :children
 
-  root "parents#index"
 
-=======
+
   namespace :admin do
     resources :users
     root to: "users#index"
@@ -15,5 +14,4 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
->>>>>>> cns_starter
 end
