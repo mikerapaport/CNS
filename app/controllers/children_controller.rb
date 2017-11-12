0bf0a5ea900d048e299a815f212c76d5bbbcd872
Laura Bunn <lbunn@colgate.edu>
2017-11-12 17:27:08 -0500

    Updated children controller

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -11,43 +11,47 @@ class ChildrenController < ApplicationController
   end
 
   def show
-    @parent = Parent.find(params[:parent_id])
     id = params[:id]
     @child = Child.find(id)
 
+    @days = ""
+    if @child.m2 || @child.m3
+      @days += "M, "
+    end
+    if @child.t2 || @child.t3
+      @days += "T, "
+    end
+    if @child.w2 || @child.w3
+      @days += "W, "
+    end
+    if @child.r2 || @child.r3
+      @days += "Th, "
+    end
+    if @child.f2 || @child.f3
+      @days += "F"
+    end
+    if @days =~ /, $/
+      @days.chop!.chop!
+    end
 
+    if @days.mtwrf
+      @days = "M, T, W, Th, F"
+    elsif @days.mwf
+      @days = "M, W, F"
+    elsif @days.tr
+      @days = "T, Th"
+    end
 
-    # @days = ""
-    # if @child.monday
-    #   @days += "M, "
-    # end
-    # if @child.tuesday
-    #   @days += "T, "
-    # end
-    # if @child.wednesday
-    #   @days += "W, "
-    # end
-    # if @child.thursday
-    #   @days += "Th, "
-    # end
-    # if @child.friday
-    #   @days += "F"
-    # end
-    # if @days =~ /, $/
-    #   @days.chop!.chop!
-    # end
-    #
-    # @hours = ""
-    # if @child.time == "Full Day"
-    #   @hours = "Full Day (7:30AM - 5:30PM)"
-    # elsif @child.time == "Half Day Morning"
-    #   @hours = "Half Day Morning (7:30AM - 12:30PM)"
-    # elsif @child.time == "Half Day Afternoon"
-    #   @hours = "Half Day Afternoon (12:30PM - 5:30PM)"
-    # elsif @child.time == "Post HCS Pre-K"
-    #   @hours = "11:30AM - 5:30"
-    # # elsif @child.time == ""
-    # end
+    @hours = ""
+    if @child.time == "Full Day"
+      @hours = "Full Day (7:30AM - 5:30PM)"
+    elsif @child.time == "Half Day Morning"
+      @hours = "Half Day Morning (7:30AM - 12:30PM)"
+    elsif @child.time == "Half Day Afternoon"
+      @hours = "Half Day Afternoon (12:30PM - 5:30PM)"
+    elsif @child.program == "Post HCS Pre-K"
+      @hours = "11:30AM - 5:30PM"
+    end
   end
 
   def create
