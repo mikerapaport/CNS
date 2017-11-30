342610c9c785366b7a60a58b7926761eff9a3fbe
Zoila Rodriguez <zoilarodriguez@Zoilas-MacBook-Pro.local>
2017-11-30 16:50:12 -0500

    Fixing Forms

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -2,11 +2,11 @@ jQuery('#child_program').on('change', function(ev) {
   // console.log(ev.target);
   var selector_val = jQuery('#child_status').val();
   var program_val = jQuery('#child_program').val();
-  if (selector_val == 'Infant') { //this needs to change; use the arg for func
+  if (selector_val == 'Infant') { 
     if (program_val == '10 Month'){
       jQuery('#infant').show();
       jQuery('#infant_summer').hide();
-    } else {
+    } else if (program_val == 'Summer') {
       jQuery('#infant_summer').show();
       jQuery('#infant').hide();
     }
@@ -18,7 +18,7 @@ jQuery('#child_program').on('change', function(ev) {
     if (program_val == '10 Month'){
       jQuery('#early_childhood').show();
       jQuery('#early_childhood_summer').hide();
-    } else {
+    } else if (program_val == 'Summer') {
       jQuery('#early_childhood_summer').show();
       jQuery('#early_childhood').hide();
     }
@@ -39,7 +39,67 @@ jQuery('#child_program').on('change', function(ev) {
     if (program_val == '10 Month'){
       jQuery('#afterschool').show();
       jQuery('#afterschool_summer').hide();
-    } else {
+    } else if (program_val == 'Summer') {
+      jQuery('#afterschool_summer').show();
+      jQuery('#afterschool').hide();
+    }
+    jQuery('#infant').hide();
+    jQuery('#infant_summer').hide();
+    jQuery('#early_childhood').hide();
+    jQuery('#early_childhood_summer').hide();
+    jQuery('#post_pre-k').hide();
+  } else {
+    jQuery('#infant').hide();
+    jQuery('infant_summer').hide();
+    jQuery('#early_childhood').hide();
+    jQuery('#early_childhood_summer').hide();
+    jQuery('#post_pre-k').hide();
+    jQuery('#afterschool').hide();
+    jQuery('#afterschool_summer').hide();
+  }
+});
+
+jQuery('#child_status').on('change', function(ev) {
+  var selector_val = jQuery('#child_status').val();
+  var program_val = jQuery('#child_program').val();
+  if (selector_val == 'Infant') {
+    if (program_val == '10 Month'){
+      jQuery('#infant').show();
+      jQuery('#infant_summer').hide();
+    } else if (program_val == 'Summer') {
+      jQuery('#infant_summer').show();
+      jQuery('#infant').hide();
+    }
+    jQuery('#early_childhood').hide();
+    jQuery('#early_childhood_summer').hide();
+    jQuery('#post_pre-k').hide();
+    jQuery('#afterschool').hide();
+  } else if (selector_val == 'Early Childhood') {
+    if (program_val == '10 Month'){
+      jQuery('#early_childhood').show();
+      jQuery('#early_childhood_summer').hide();
+    } else if (program_val == 'Summer') {
+      jQuery('#early_childhood_summer').show();
+      jQuery('#early_childhood').hide();
+    }
+    jQuery('#infant').hide();
+    jQuery('#infant_summer').hide();
+    jQuery('#post_pre-k').hide();
+    jQuery('#afterschool').hide();
+  } else if (selector_val == 'Post HCS Pre-K') {
+    if (program_val == '10 Month') {
+      jQuery('#post_pre-k').show();
+    }
+    jQuery('#infant').hide();
+    jQuery('#infant_summer').hide();
+    jQuery('#early_childhood').hide();
+    jQuery('#early_childhood_summer').hide();
+    jQuery('#afterschool').hide();
+  } else if (selector_val == 'HCS After-school') {
+    if (program_val == '10 Month'){
+      jQuery('#afterschool').show();
+      jQuery('#afterschool_summer').hide();
+    } else if (program_val == 'Summer') {
       jQuery('#afterschool_summer').show();
       jQuery('#afterschool').hide();
     }
